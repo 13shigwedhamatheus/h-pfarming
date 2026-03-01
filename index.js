d945f7db1481cfed851663a6d88842b283518b01
Shigwedha Matheus <shigwedhamatheus13@gmail.com>
2026-03-01 17:37:31 +0200

    Counter imrovements on the nav and body

--- index.js
+++ index.js
@@ -5,28 +5,50 @@ copyright.textContent = new Date().getFullYear();
 
 
 //Hero header nav About drop-down menu
-function openNav(){
-
+function openDropDown(){
+    const drop = document.querySelector(".dropdown");
     //document.getElementById("main-drop").addEventListener("mouseenter", function (){
-    document.querySelector(".dropdown").style.display = "block";
+    if (drop.style.display == "block"){
+        document.querySelector(".dropdown").style.display = "none";
+    }
+    else{
+        document.querySelector(".dropdown").style.display = "block";
+    }    
     //})
 }
 
-function closeNav(){
+/*function closeNav(){
 
     //document.getElementById("main-drop").addEventListener("mouseleave", function (){
         document.querySelector(".dropdown").style.display = "none";
     //})
-}
+}*/
 
 //@media screens menu button activations
 
 //opening the menu button
-document.getElementById("open-menu-btn").addEventListener('click',() =>{
+/*document.getElementById("open-menu-btn").addEventListener('click',() =>{
     document.querySelector("#hero header nav ul").style.display = "block";
-})
+})*/
+function openNav(){
+
+    const nav = document.querySelector('#hero header nav ul');
+    const opensvg = 'align-justify.svg';
+    const closesvg = 'xmark.svg';
+    const navbtnsvg = document.getElementById('navsvg');
+
+    if (nav.style.display == 'block'){
+        document.querySelector("#hero header nav ul").style.display = "none";
+        navbtnsvg.setAttribute('src',opensvg)
+    }
+    else{
+        document.querySelector("#hero header nav ul").style.display = "block";
+        navbtnsvg.setAttribute('src',closesvg)
+        
+    }
+}
 
 //closing the menu button
-document.getElementById("close-menu-btn").addEventListener("click", () =>{
+/*document.getElementById("close-menu-btn").addEventListener("click", () =>{
     document.querySelector("#hero header nav ul").style.display = "none";
-})
+})*/
